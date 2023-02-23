0ff5dc863576cc8e0e68f8fa25f497b065ddd04d
vara0715 <88980746+vara0715@users.noreply.github.com>
2023-02-23 08:10:02 +0530

    Add files via upload

---
+++ ds-numpy-5.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+ary = np.random.rand(10,10)  # This will give random float values
+max = np.max(ary)
+min = np.min(ary)
+print("Array :", ary)
+print("Max Value :", max)
+print("Min Value :", min)
